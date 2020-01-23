171d7367c619aef220bfb978200d77d0763be4c3
TarzanKeyz <dannybedard1@gmail.com>
2020-01-23 16:42:54 -0500

    un autre envoie

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int [] tabInt = {34, 89, 56, 9000, 876, 32458, 5759};
-        String [] tabStr = {"Bonjours", "Salut", "Seymour", "AudreyII", "Singapour", "Contestant"};
+        int [] tabInt = {1, 3, 8, 5, 34, 6, 4, 9, -56};
+       // String [] tabStr = {"6", "9", "5", "2", "4", "3"};
 
-        Tableaux.trierCroissantSSS(tabInt);
-        Tableaux.trierCroissantSSS(tabStr);
+        //Tableaux.trierCroissantSSS(tabInt);
+        //Tableaux.trierCroissantSSS(tabStr);
+        Tableaux.trierDecroissant(tabInt);
+
+        //Tableaux.print(tabInt);
+
+        System.out.println(Tableaux.maximum(tabInt));
+        System.out.println(Tableaux.minimum(tabInt));
     }
 }
--- Tableaux.java
+++ Tableaux.java
@@ -40,4 +40,42 @@ public class Tableaux {
                 permute(tab, i, imin);
         }
     }
+
+    public static void trierDecroissant(int[] tab){
+        int imax;
+        for (int i=0;i<tab.length-1;i++){
+            imax = i;
+            for (int j=0; j<tab.length; j++){
+                if (tab[j] > tab[imax])
+                    imax = j;
+            }
+            if (imax != i){
+                permute(tab, i, imax);
+            }
+        }
+    }
+
+    public static void print (int [] tab){
+        for (int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+    }
+
+    public static int maximum (int [] tab){
+        int max = tab[0];
+        for(int i=0; i<tab.length-1; i++){
+            if(tab[i] > max)
+            max = tab[i];
+        }
+        return max;
+    }
+
+    public static int minimum (int [] tab){
+        int min = tab [0];
+        for (int i=0; i<tab.length;i++){
+            if (tab[i] < min)
+                min = tab[i];
+        }
+        return min;
+    }
 }
